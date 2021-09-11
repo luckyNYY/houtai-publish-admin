5ebec9b9fdc89101c96cb24153773df5d6912972
luckyNYY <nongyanyu729@163.com>
2021-09-11 15:39:29 +0800

    添加全局样式index.less文件，创建根目录vue.config.js文件添加less，添加less至main.js文件

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+
+    pluginOptions: {
+
+        ​
+        "style-resources-loader": {
+
+            ​ preProcessor: "less",
+
+            ​patterns: []
+
+            ​ }
+
+    }
+
+};
